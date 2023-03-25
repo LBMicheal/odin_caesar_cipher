f8ff7eb5554c6b148cfd59f14c9ced15bb8ebece
LBMicheal <bohlmeyerm@gmail.com>
2023-03-24 21:55:29 -0500

    First commit, project built

---
+++ script.rb
@@ -0,0 +1,45 @@
+string = "By the way"
+shift = 3
+
+char_array = string.split("")
+int_array = []
+cypher_array = []
+
+char_array.each do |char|
+  int_array.push(char.ord)
+end
+
+def convert_to_cipher (integer, shift)
+  temp = 0
+  unless integer == 33
+    p integer
+    case integer
+    when 65..90
+      integer += shift
+      if integer > 90
+        temp = integer - 90
+        integer = 64 + temp
+      end
+    when 97..122
+      integer += shift
+      if integer > 122
+        temp = integer - 122
+        integer = 96 + temp
+      end
+    end
+    p integer
+  end
+end
+
+int_array.map! do |int|
+    convert_to_cipher(int, shift)
+end
+
+int_array.each do |int|
+  cypher_array.push(int.chr)
+end
+
+p string
+p char_array
+p int_array
+p cypher_array.join
